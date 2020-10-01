1d6764f3e538716d4d6aa013dcb933f20cc190fe
ashley.bishop <ashley.bishop@gamesys.co.uk>
2020-10-01 22:42:39 +0100

    .

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 var app = new PIXI.Application({
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width: 1400,
+    height: 800,
     backgroundColor: 0x2c3e50,
     //forceCanvas:true
 });
@@ -14,12 +14,15 @@ function startup()
 {
     const container = new PIXI.Container();
     var bunny = new PIXI.Sprite(app.loader.resources.bunny.texture);
+    bunny.anchor.set(0.5)
     container.addChild (bunny);
     var g = new PIXI.Graphics ()
-        .drawRect (1800,980,1000,100);
+        .drawRect (500,400,1000,100);
 
     container.mask = g;
     container.addChild (g);
+
+    //app.stage.addChild (g);
 
     bunny.scale.set (10, 10);
     bunny.x = app.renderer.width / 2;
